07f5b0d0b43f000853ac2a871605cc6e2e2f5d84
gedomel <gedomel2@illinois.edu>
2020-11-23 13:06:00 -0600

    brightness slider functionality

--- app/src/main/java/com/pstcstest/paletteprovider/BrightnessActivity.java
+++ app/src/main/java/com/pstcstest/paletteprovider/BrightnessActivity.java
@@ -1,14 +1,15 @@
 package com.pstcstest.paletteprovider;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import com.pstcstest.paletteprovider.R;
-
 public class BrightnessActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,24 @@ public class BrightnessActivity extends AppCompatActivity{
         setContentView(R.layout.activity_brightness);
         ImageButton HomeButtonBrightness = (ImageButton) findViewById(R.id.homebutton_brightness_photo);
         ImageButton BackButtonBrightness = (ImageButton) findViewById(R.id.backbutton_brightness_photo);
-
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.alma465);
+        ImageView imageView = (ImageView) findViewById(R.id.editPhotoImage);
+        imageView.setImageBitmap(bitmap);
+        PictureThread pictureThread = new PictureThread(imageView, bitmap);
+        pictureThread.start();
+        SeekBar seekbar = (SeekBar) findViewById(R.id.seekbar);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                pictureThread.adjustBrightness(seekBar.getProgress()-255);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
         BackButtonBrightness.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +55,5 @@ public class BrightnessActivity extends AppCompatActivity{
     private void startEditActivity() {
         Intent intent = new Intent(this, EditPhotoActivity.class);
         startActivity(intent);
-
     }
 }
