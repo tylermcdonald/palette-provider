62e0515b82ed8b254d28eb491034263cc97feb99
gedomel <gedomel2@illinois.edu>
2020-11-23 14:50:46 -0600

    button for brightness and zoom

--- app/src/main/java/com/pstcstest/paletteprovider/EditPhotoActivity.java
+++ app/src/main/java/com/pstcstest/paletteprovider/EditPhotoActivity.java
@@ -1,20 +1,22 @@
 package com.pstcstest.paletteprovider;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-
+import android.widget.ImageView;
 public class EditPhotoActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_photo);
-
+        ImageView imageView = findViewById(R.id.editPhotoImage);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            int resId = bundle.getInt("resId");
+            imageView.setImageResource(resId);
+        }
         ImageButton HomeButton = (ImageButton) findViewById(R.id.homebutton_edit_photo);
         ImageButton BackToExtractButton = (ImageButton) findViewById(R.id.backbutton_edit_photo);
         Button ContinueButton = (Button) findViewById(R.id.continueButton);
@@ -37,8 +39,6 @@ public class EditPhotoActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startExtractColorPhotoActivity();
             }
-
-
         });
         ZoomButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,11 +64,85 @@ public class EditPhotoActivity extends AppCompatActivity {
     private void startZoomActivity() {
         Intent intent = new Intent(this, ZoomActivity.class);
         startActivity(intent);
-
     }
     private void startBrightnessActivity() {
         Intent intent = new Intent(this, BrightnessActivity.class);
         startActivity(intent);
-
     }
 }
+
+
+//package com.pstcstest.paletteprovider;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.ImageButton;
+//
+//public class EditPhotoActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_edit_photo);
+//
+//        ImageButton HomeButton = (ImageButton) findViewById(R.id.homebutton_edit_photo);
+//        ImageButton BackToExtractButton = (ImageButton) findViewById(R.id.backbutton_edit_photo);
+//        Button ContinueButton = (Button) findViewById(R.id.continueButton);
+//        Button ZoomButton = (Button) findViewById(R.id.zoomButton);
+//        Button BrightnessButton = (Button) findViewById(R.id.brightnessButton);
+//        HomeButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startHomeScreenActivity();
+//            }
+//        });
+//        BackToExtractButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startHomeScreenActivity();
+//            }
+//        });
+//        ContinueButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startExtractColorPhotoActivity();
+//            }
+//
+//
+//        });
+//        ZoomButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startZoomActivity();
+//            }
+//        });
+//        BrightnessButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startBrightnessActivity();
+//            }
+//        });
+//    }
+//    private void startHomeScreenActivity() {
+//        Intent intent = new Intent(this, HomeActivity.class);
+//        startActivity(intent);
+//    }
+//    private void startExtractColorPhotoActivity() {
+//        Intent intent = new Intent(this, ExtractColorPhotoActivity.class);
+//        startActivity(intent);
+//    }
+//    private void startZoomActivity() {
+//        Intent intent = new Intent(this, ZoomActivity.class);
+//        startActivity(intent);
+//
+//    }
+//    private void startBrightnessActivity() {
+//        Intent intent = new Intent(this, BrightnessActivity.class);
+//        startActivity(intent);
+//
+//    }
+//}
--- app/src/main/java/com/pstcstest/paletteprovider/ZoomActivity.java
+++ app/src/main/java/com/pstcstest/paletteprovider/ZoomActivity.java
@@ -1,17 +1,12 @@
 package com.pstcstest.paletteprovider;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-
 import com.pstcstest.paletteprovider.R;
-
 public class ZoomActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +25,87 @@ public class ZoomActivity extends AppCompatActivity {
                 startEditPhotoActivity();
             }
         });
-
+        ImageButton pic1 = (ImageButton) findViewById(R.id.Button01);
+        ImageButton pic2 = (ImageButton) findViewById(R.id.Button02);
+        ImageButton pic3 = (ImageButton) findViewById(R.id.Button03);
+        ImageButton pic4 = (ImageButton) findViewById(R.id.Button04);
+        ImageButton pic5 = (ImageButton) findViewById(R.id.Button05);
+        ImageButton pic6 = (ImageButton) findViewById(R.id.Button06);
+        ImageButton pic7 = (ImageButton) findViewById(R.id.Button07);
+        ImageButton pic8 = (ImageButton) findViewById(R.id.Button08);
+        ImageButton pic9 = (ImageButton) findViewById(R.id.Button09);
+        pic1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZoomActivity.this, EditPhotoActivity.class);
+                intent.putExtra("resId", R.drawable.image_part_001);
+                startActivity(intent);
+            }
+        });
+        pic2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZoomActivity.this, EditPhotoActivity.class);
+                intent.putExtra("resId", R.drawable.image_part_002);
+                startActivity(intent);
+            }
+        });
+        pic3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZoomActivity.this, EditPhotoActivity.class);
+                intent.putExtra("resId", R.drawable.image_part_003);
+                startActivity(intent);
+            }
+        });
+        pic4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZoomActivity.this, EditPhotoActivity.class);
+                intent.putExtra("resId", R.drawable.image_part_004);
+                startActivity(intent);
+            }
+        });
+        pic5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZoomActivity.this, EditPhotoActivity.class);
+                intent.putExtra("resId", R.drawable.image_part_005);
+                startActivity(intent);
+            }
+        });
+        pic6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZoomActivity.this, EditPhotoActivity.class);
+                intent.putExtra("resId", R.drawable.image_part_006);
+                startActivity(intent);
+            }
+        });
+        pic7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZoomActivity.this, EditPhotoActivity.class);
+                intent.putExtra("resId", R.drawable.image_part_007);
+                startActivity(intent);
+            }
+        });
+        pic8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZoomActivity.this, EditPhotoActivity.class);
+                intent.putExtra("resId", R.drawable.image_part_008);
+                startActivity(intent);
+            }
+        });
+        pic9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ZoomActivity.this, EditPhotoActivity.class);
+                intent.putExtra("resId", R.drawable.image_part_009);
+                startActivity(intent);
+            }
+        });
     }
     private void startHomeScreenActivity() {
         Intent intent = new Intent(this, HomeActivity.class);
@@ -41,3 +116,47 @@ public class ZoomActivity extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
+//package com.pstcstest.paletteprovider;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.ImageButton;
+//
+//import com.pstcstest.paletteprovider.R;
+//
+//public class ZoomActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_crop);
+//        ImageButton HomeButton = (ImageButton) findViewById(R.id.homebutton_zoom_photo);
+//        ImageButton BackToExtractButton = (ImageButton) findViewById(R.id.backbutton_zoom_photo);
+//        HomeButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startHomeScreenActivity();
+//            }
+//        });
+//        BackToExtractButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startEditPhotoActivity();
+//            }
+//        });
+//
+//    }
+//    private void startHomeScreenActivity() {
+//        Intent intent = new Intent(this, HomeActivity.class);
+//        startActivity(intent);
+//    }
+//    private void startEditPhotoActivity() {
+//        Intent intent = new Intent(this, EditPhotoActivity.class);
+//        startActivity(intent);
+//    }
+//}
